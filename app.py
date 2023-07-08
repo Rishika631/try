85920d6fd743f737bf57f89fcf19ad6bd6d69a2b
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-08 23:54:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,24 @@ import streamlit as st
 import requests
 from PIL import Image
 import io
+import base64
 
 API_URL = "https://api-inference.huggingface.co/models/to-be/donut-base-finetuned-invoices"
 headers = {"Authorization": "Bearer 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"}
 
+def image_to_base64(image):
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+    encoded_image = base64.b64encode(img_byte_arr).decode('utf-8')
+    return encoded_image
+
 def query(image):
-    response = requests.post(API_URL, headers=headers, files={"file": image})
+    encoded_image = image_to_base64(image)
+    data = {
+        'file': encoded_image
+    }
+    response = requests.post(API_URL, headers=headers, json=data)
     return response.text
 
 def main():
@@ -29,13 +41,13 @@ def main():
             image = Image.open(image_path)
             st.image(image, use_column_width=True)
             if st.button(f"Use Image {index+1}", key=f"use_image_{index}"):
-                with open(image_path, "rb") as f:
-                    output = query(f.read())  # Read the file contents
+                output = query(image)
                 st.write("Extracted Text:")
                 st.write(output)
 
     if uploaded_file is not None:
-        output = query(uploaded_file.read())  # Read the file contents
+        uploaded_image = Image.open(uploaded_file)
+        output = query(uploaded_image)
         st.write("Extracted Text:")
         st.write(output)
 
