375e3459719a0169ea7507b9ea55da320b7a2ce4
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-08 22:04:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/to-be/donut-base-finetuned-invoices"
+headers = {"Authorization": "Bearer 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"}
+
+def query(file):
+    response = requests.post(API_URL, headers=headers, data=file)
+    return response.text
+
+def main():
+    st.title("Documented Form Text Extraction")
+    st.write("Upload an image of a handwritten form to extract the text.")
+
+    sample_images = [
+        "https://example.com/sample_image_1.jpg",
+        "https://example.com/sample_image_2.jpg",
+        "https://example.com/sample_image_3.jpg"
+    ]
+
+    uploaded_file = st.file_uploader("Upload Image", type=['jpg', 'jpeg', 'png'])
+
+    st.subheader("Select a Sample Image:")
+    for image_url in sample_images:
+        col = st.beta_columns(2)
+        with col[0]:
+            image = st.image(image_url, use_column_width=True)
+            if image.button("Use this Image"):
+                output = query(requests.get(image_url).content)
+                st.write("Extracted Text:")
+                st.write(output)
+
+    if uploaded_file is not None:
+        output = query(uploaded_file.read())
+        st.write("Extracted Text:")
+        st.write(output)
+
+if __name__ == "__main__":
+    main()
