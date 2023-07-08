8950eba292cd4fa343a97e885b4d4a8601920ce7
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-09 00:02:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import requests
 from PIL import Image
+import io
 
 API_URL = "https://api-inference.huggingface.co/models/to-be/donut-base-finetuned-invoices"
 headers = {"Authorization": "Bearer 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"}
 
-def query(image):
-    response = requests.post(API_URL, headers=headers, files={"file": image})
+def query(image_bytes):
+    response = requests.post(API_URL, headers=headers, data=image_bytes)
     return response.text
 
 def main():
@@ -29,12 +30,14 @@ def main():
             st.image(image, use_column_width=True)
             if st.button(f"Use Image {index+1}", key=f"use_image_{index}"):
                 with open(image_path, "rb") as f:
-                    output = query(f.read())
+                    image_bytes = f.read()
+                output = query(image_bytes)
                 st.write("Extracted Text:")
                 st.write(output)
 
     if uploaded_file is not None:
-        output = query(uploaded_file.read())
+        image_bytes = uploaded_file.read()
+        output = query(image_bytes)
         st.write("Extracted Text:")
         st.write(output)
 
