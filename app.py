dbd857c4e99c4402d30113a37bb3ad8a7404b27c
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-10 14:38:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import cv2 as cv
 import streamlit as st
-import numpy as np
-import pafy
+from pytube import YouTube
 from PIL import Image
+import imageio
 
 # Streamlit app
 def main():
@@ -25,32 +24,29 @@ def main():
         else:
             st.warning("Please enter a YouTube video link")
 
-# Extract key frames from the video using OpenCV
+# Extract key frames from the video using pytube3 and imageio
 def extract_key_frames(youtube_link):
-    video = pafy.new(youtube_link)
-    best = video.getbest(preftype="mp4")
-    cap = cv2.VideoCapture(best.url)
-
+    yt = YouTube(youtube_link)
+    video = yt.streams.get_highest_resolution()
+    video_path = video.download()
     key_frames = []
-    frame_count = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        if frame_count % 60 == 0:  # Change key frame interval as desired
-            key_frames.append(frame)
-        frame_count += 1
 
-    cap.release()
+    # Read key frames using imageio
+    reader = imageio.get_reader(video_path)
+    for idx, frame in enumerate(reader):
+        # Extract key frames at desired intervals
+        if idx % 60 == 0:
+            key_frames.append(frame)
+
     return key_frames
 
 # Display the key frames as an image summary
 def display_summary(key_frames):
     st.subheader("Video Image Summary")
     if key_frames:
-        for key_frame in key_frames:
+        for idx, key_frame in enumerate(key_frames):
             image = Image.fromarray(key_frame)
-            st.image(image, caption="Key Frame")
+            st.image(image, caption=f"Key Frame {idx+1}")
     else:
         st.warning("No key frames found in the video")
 
